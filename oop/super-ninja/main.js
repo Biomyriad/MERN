5329ddf7cc20c4020a6efb0bc16132bd72bfee0a
Thomas Kosta <biomyriad@gmail.com>
2022-08-21 11:11:02 -0700

    commit

---
+++ oop/super-ninja/main.js
@@ -0,0 +1,43 @@
+class Ninja {
+    constructor (name, health, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats () {
+        console.log(this.name)
+        console.log(this.health)
+        console.log(this.speed)
+        console.log(this.strength)
+    }
+
+    drinkSake () {
+        this.health = this.health + 10;
+    }
+}
+
+
+class Sensei extends Ninja {
+    constructor (name, health = 200, speed = 10, strength = 10, wisdom = 10) {
+        super(name, health, speed, strength)
+        this.wisdom
+    }
+
+    speakWisdom () {
+        this.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months");
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
