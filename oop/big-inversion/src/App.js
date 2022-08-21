af5c504df0f185bfb1c46bbf2d43eeb893d25255
Thomas Kosta <biomyriad@gmail.com>
2022-08-21 15:45:25 -0700

    commit

---
+++ oop/big-inversion/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PersonCard from './components/PersonCard.js'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
+        <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
+        <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"/>
+        <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor="Brown"/>
+      </div>
+    )
+  }
+}
